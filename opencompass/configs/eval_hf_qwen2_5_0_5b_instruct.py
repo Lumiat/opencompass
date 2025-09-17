f06c53530e8115ecaa39bf6761e88edd30f68b1f
Lumiat <grace20177102@icloud,com>
2025-09-17 23:48:36 +0800

    modify eval_hf_qwen2_5_0_5b_instruct.py

--- opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py
+++ opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py
@@ -1,19 +1,43 @@
-from mmengine.config import read_base
-with read_base():
-    from .datasets.ARC_c.ARC_c_test_gen import ARC_c_datasets
+from opencompass.openicl.icl_prompt_template import PromptTemplate
+from opencompass.openicl.icl_retriever import ZeroRetriever
+from opencompass.openicl.icl_inferencer import GenInferencer
+from opencompass.openicl.icl_evaluator import AccEvaluator
+from opencompass.datasets import ARCDataset
+from opencompass.utils.text_postprocessors import first_option_postprocess
 
-datasets = [*ARC_c_datasets]
+ARC_c_reader_cfg = dict(
+    input_columns=['question', 'textA', 'textB', 'textC', 'textD'],
+    output_column='answerKey')
 
-from opencompass.models import HuggingFacewithChatTemplate
+ARC_c_infer_cfg = dict(
+    prompt_template=dict(
+        type=PromptTemplate,
+        template=dict(
+            round=[
+                dict(
+                    role='HUMAN',
+                    prompt='you are a helpful AI assistant, and you are going to answer the question of the user by picking one answer among the given choices. Answer the chapital character of the choice directly. You\'ll only need to answer by a single [ans] (ans is A,B,C,D or True/False)\n\n{question}\nA: {textA}\nB: {textB}\nC: {textC}\nD: {textD}'
+                )
+            ], ),
+    ),
+    retriever=dict(type=ZeroRetriever),
+    inferencer=dict(type=GenInferencer),
+)
 
-models = [
+ARC_c_eval_cfg = dict(
+    evaluator=dict(type=AccEvaluator),
+    pred_role='BOT',
+    pred_postprocessor=dict(type=first_option_postprocess, options='ABCD'),
+)
+
+ARC_c_datasets = [
     dict(
-        type=HuggingFacewithChatTemplate,
-        abbr='qwen2.5-0.5b-instruct-hf',
-        path='/research-intern02/xjy/ParaGen-Dataset/models/Qwen2.5-0.5B-Instruct',
-        peft_path='/research-intern02/xjy/ParaGen-Dataset/saves/common_sense_reasoning/ARC-c/Qwen2.5-0.5B-Instruct_lora-rank_8_finetune/checkpoint-749_adapter_model.safetensors',
-        max_out_len=4096,
-        batch_size=8,
-        run_cfg=dict(num_gpus=1),
+        abbr='ARC-c-test-gen',
+        type=ARCDataset,
+        path='opencompass/ai2_arc-dev',
+        name='ARC-Challenge',
+        reader_cfg=ARC_c_reader_cfg,
+        infer_cfg=ARC_c_infer_cfg,
+        eval_cfg=ARC_c_eval_cfg,
     )
 ]
