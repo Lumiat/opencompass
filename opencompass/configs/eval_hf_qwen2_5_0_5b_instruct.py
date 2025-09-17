7b8d452cf66c1095c1ce586436f8ddf5f8eb7cdb
Lumiat <grace20177102@icloud,com>
2025-09-18 00:15:21 +0800

    Revert "modify model path"
    This reverts commit 034c490f514b9336cb62f29ff5736eb3f9df0d91.
    
    revert changes to opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py

--- opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py
+++ opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py
@@ -10,7 +10,7 @@ models = [
     dict(
         type=HuggingFacewithChatTemplate,
         abbr='qwen2.5-0.5b-instruct-hf',
-        path='/research-intern02/xjy/ParaGen-Dataset/models/Qwen2.5-0.5B-Instruct',
+        path='Qwen/Qwen2.5-0.5B-Instruct',
         peft_path='/research-intern02/xjy/ParaGen-Dataset/saves/common_sense_reasoning/ARC-c/Qwen2.5-0.5B-Instruct_lora-rank_8_finetune/checkpoint-749_adapter_model.safetensors',
         max_out_len=4096,
         batch_size=8,
