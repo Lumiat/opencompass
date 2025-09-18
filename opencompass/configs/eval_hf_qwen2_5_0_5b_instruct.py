ab78e1551457084505822937058625e89c8d20b2
Lumiat <grace20177102@icloud.com>
2025-09-18 16:49:34 +0000

    :white_check_mark: try to test multiple model

--- opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py
+++ opencompass/configs/eval_hf_qwen2_5_0_5b_instruct.py
@@ -4,25 +4,27 @@ with read_base():
     from .datasets.ARC_c.ARC_c_test_ppl import ARC_c_datasets_ppl
 
 datasets = [*ARC_c_datasets_gen, *ARC_c_datasets_ppl]
+# datasets = [*ARC_c_datasets_gen]
 
-from opencompass.models import HuggingFacewithChatTemplate
+# from opencompass.models import HuggingFacewithChatTemplate
+from opencompass.models import HuggingFaceCausalLM
 
 models = [
     dict(
-        type=HuggingFacewithChatTemplate,
-        abbr='qwen2.5-0.5b-instruct-hf',
+        type=HuggingFaceCausalLM,
+        abbr='qwen2.5-0.5b-instruct-hf-rank8-checkpoint001',
         path='/research-intern02/xjy/ParaGen-Dataset/models/Qwen2.5-0.5B-Instruct',
         peft_path='/research-intern02/xjy/ParaGen-Dataset/saves/common_sense_reasoning/ARC-c/Qwen2.5-0.5B-Instruct_lora-rank_8_finetune/checkpoint-100',
-        max_out_len=4096,
+        max_out_len=128, # original: 4096
         batch_size=8,
         run_cfg=dict(num_gpus=1),
     ),
     dict(
-        type=HuggingFacewithChatTemplate,
-        abbr='qwen2.5-0.5b-instruct-hf',
+        type=HuggingFaceCausalLM,
+        abbr='qwen2.5-0.5b-instruct-hf-rank8-checkpoint002',
         path='/research-intern02/xjy/ParaGen-Dataset/models/Qwen2.5-0.5B-Instruct',
         peft_path='/research-intern02/xjy/ParaGen-Dataset/saves/common_sense_reasoning/ARC-c/Qwen2.5-0.5B-Instruct_lora-rank_8_finetune/checkpoint-002',
-        max_out_len=4096,
+        max_out_len=128, # original: 4096
         batch_size=8,
         run_cfg=dict(num_gpus=1),
     ),
